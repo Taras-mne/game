d0edc8b2ea3eed434283d19cd7102d0641e20477
Taras Sofronov <taras.sofronov.mne@gmail.com>
2026-03-02 19:10:06 +0100

    draw_tilemap in rendering sys

--- rendering_system.lua
+++ rendering_system.lua
@@ -1,3 +1,20 @@
 function draw_tile(tile, x, y)
     love.graphics.draw(TILE_ATLASES[tile.atlas], tile.quad, x, y)
 end
+
+function draw_tilemap(tilemap, x, y)
+    local tiles = tilemap.tiles
+    local v_sh = 0
+    for _, column in ipairs(tiles) do
+        local h_sh = 0
+        for _, value in ipairs(column) do
+            local tile = TILESET[value]
+            if tile then
+                draw_tile(tile, h_sh + x, v_sh + y)
+            end
+
+            h_sh = h_sh + TILE_SIZE.w
+        end
+        v_sh = v_sh + TILE_SIZE.h
+    end
+end
