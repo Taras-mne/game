90d40ae54c4a999481d35e8ea98c7e27300f5e2d
Taras Sofronov <taras.sofronov.mne@gmail.com>
2026-04-02 11:33:38 +0200

    add_keys_callback implemented

--- key_mapping_system.lua
+++ key_mapping_system.lua
@@ -4,6 +4,12 @@ function add_key_callback(key, call)
     KEYS_CALLBACKS[key] = call
 end
 
+function add_keys_callback(keys, call)
+    for i,key in ipairs(keys) do
+        KEYS_CALLBACKS[key] = call
+    end 
+end
+
 function love.keypressed(key, scancode, isrepeat)
     if KEYS_CALLBACKS[key] then
         KEYS_CALLBACKS[key](key, scancode, isrepeat)
